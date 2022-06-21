6f6b884483c30ce3712129b9a421e12af287c8d8
Phil <phil.nice.23@gmail.com>
2022-06-21 11:09:32 +0300

    add List and db.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,52 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 import styles from './App.module.css';
 
-const App = () => {
-	return <div className={styles.app}></div>;
+const App = (): JSX.Element => {
+	const [error, setError] = useState(null);
+	const [isLoaded, setIsLoaded] = useState(false);
+	const [cards, setCards] = useState([]);
+
+	useEffect(() => {
+		fetch('/api/db.json')
+			.then((res) => res.json())
+			.then(
+				(result) => {
+					setIsLoaded(true);
+					setCards(result.cards);
+				},
+				(error) => {
+					setIsLoaded(true);
+					setError(error);
+				},
+			);
+	}, []);
+
+	interface ICard {
+		id: number;
+		name: string;
+		price: string;
+	}
+
+	const listItems = cards.map((item: ICard) => {
+		return (
+			<li key={item.id}>
+				{item.name} {item.price}
+			</li>
+		);
+	});
+
+	if (error) {
+		return <div>Ошибка</div>;
+	} else if (!isLoaded) {
+		return <div>Загрузка...</div>;
+	} else {
+		return (
+			<div className={styles.app}>
+				<ul>{listItems}</ul>
+			</div>
+		);
+	}
 };
 
 export default App;
---
+++ src/components/List/List.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './List.module.css';
+
+const List = (): JSX.Element => {
+	return <div className={styles.list}>List</div>;
+};
+
+export default List;
